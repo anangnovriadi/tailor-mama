501fbc370bb88798ff65c8075292ec6f55376ed5
anangnovriadi <anangnov99@gmail.com>
2018-05-01 23:18:21 +0700

    sidebar modify

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -6,20 +6,45 @@
                     <span>MAIN NAVIGATION</span>
                 </li>
                 <li>
-                    <a href="#dashboard" title="Dashboard" data-toggle="collapse" class="menu-toggle">
+                    <a href="/" title="Dashboard">
                         <em class="material-icons">dashboard</em>
                         <span>Dashboard</span>
                     </a>
-                    <ul id="dashboard" class="nav sidebar-subnav collapse">
-                        <li class="sidebar-subnav-header">Dashboard</li>
+                </li>
+                <li>
+                    <a href="#jahitan" title="Jahitan" data-toggle="collapse" class="menu-toggle">
+                        <em class="material-icons">store</em>
+                        <span>Jahitan</span>
+                    </a>
+                    <ul id="jahitan" class="nav sidebar-subnav collapse">
+                        <li class="sidebar-subnav-header">Jahitan</li>
                         <li>
-                            <a href="./index.html" title="Jahitan">
-                                <span>Jahitan</span>
+                            <a href="#" title="Customer">
+                                <span>Add Jahitan</span>
                             </a>
                         </li>
-                        <li class=" active">
+                        <li>
+                            <a href="#" title="Jahitan">
+                                <span>Data Jahitan</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#customer" title="Jahitan" data-toggle="collapse" class="menu-toggle">
+                        <em class="material-icons">perm_identity</em>
+                        <span>Customer</span>
+                    </a>
+                    <ul id="customer" class="nav sidebar-subnav collapse">
+                        <li class="sidebar-subnav-header">Customer</li>
+                        <li>
+                            <a href="{{ route('customer.index') }}" title="Customer">
+                                <span>Data Customer</span>
+                            </a>
+                        </li>
+                        <li>
                             <a href="{{ route('customer.create') }}" title="Customer">
-                                <span>Customer</span>
+                                <span>Add Customer</span>
                             </a>
                         </li>
                     </ul>
