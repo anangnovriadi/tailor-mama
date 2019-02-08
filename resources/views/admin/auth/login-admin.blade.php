a47889cd3c91c2dfbbc35b25115c880868210a10
anangnov99@gmail.com <anangnov99@gmail.com>
2019-02-08 17:37:54 +0700

    error msg

--- resources/views/admin/auth/login-admin.blade.php
+++ resources/views/admin/auth/login-admin.blade.php
@@ -22,7 +22,7 @@
                             <i class="material-icons">person</i>
                         </span>
                         <div class="form-line">
-                            <input type="text" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
+                            <input type="text" class="form-control" name="email" placeholder="Email" required />
                         </div>
                     </div>
                     <div class="input-group addon-line">
@@ -30,13 +30,18 @@
                             <i class="material-icons">lock</i>
                         </span>
                         <div class="form-line">
-                            <input type="password" class="form-control" name="password" placeholder="Password" required="" />
+                            <input type="password" class="form-control" name="password" placeholder="Password" required />
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xs-6 p-t-5">
-                            {{--  --}}
-                        </div>
+                        @if (session()->has('errors'))
+                            <div class="col-xs-12 p-t-5">
+                                <div class="alert dark alert-danger alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                    <strong>Error!</strong> Email and password is incorrect.
+                                </div>
+                            </div>
+                        @endif
                     </div>
                     <button class="btn btn-block btn-primary waves-effect" type="submit">LOG IN</button>
                 </form>
